8302c188c3295cb58f07d1834df68938a97bf476
hyperchessbot <hyperchessbot@gmail.com>
2021-01-15 13:03:23 +0000

    Redirect to site

--- functions/callback.js
+++ functions/callback.js
@@ -1,4 +1,5 @@
 const soa = require('simple-oauth2')
+const fetch = require('node-fetch')
 
 const client = soa.create({
     client: {
@@ -13,28 +14,64 @@ const client = soa.create({
 })
 
 exports.handler = (event, context, callback) => {
-  const code = event.queryStringParameters.code
-  const state = event.queryStringParameters.state
+    const code = event.queryStringParameters.code
+    const state = event.queryStringParameters.state
 
-  client.authorizationCode.getToken({
-    code: code,
-    redirect_uri: process.env.CALLBACK_URL,    
-  })
-    .then(result => {
-      const token = result.token
-      return callback(null, {
-        statusCode: 200,
-        body: JSON.stringify(result)
-      })
-    })    
+    client.authorizationCode.getToken({
+        code: code,
+        redirect_uri: process.env.CALLBACK_URL,    
+    }).then(result => {
+        console.log("result", result)
+
+        const token = result.access_token
+
+        const url = `https://lichess.org/api/account`
+
+        let config = {
+            headers:{
+                Authorization: `Bearer ${token}`
+            }
+        }
+
+        console.log("fetching", url, config)      
+
+        fetch(url, config).then(
+            response => response.json().then(
+                json => {
+                    console.log(json)
+                    return callback(null, {
+                        statusCode: 302,
+                        headers: {
+                            Location: `https://amgilp.herokuapp.com?getpuzzles=${json.username}`,
+                            'Cache-Control': 'no-cache'
+                        },
+                        body: ''
+                    })
+                },
+            err => {
+                console.log(err)
+                return callback(null, {
+                    statusCode: error.statusCode || 500,
+                    body: `json parsing error`
+                })
+            }   
+        ),
+        err => {
+            console.log("fetch error", err)
+            return callback(null, {
+                statusCode: error.statusCode || 500,
+                body: `fetch error`
+            })
+        }
+    )})
     .catch((error) => {
-      console.log('Access Token Error', error.message)
-      console.log(error)
-      return callback(null, {
-        statusCode: error.statusCode || 500,
-        body: JSON.stringify({
-          error: error.message,
+        console.log('Access Token Error', error.message)
+        console.log(error)
+        return callback(null, {
+                statusCode: error.statusCode || 500,
+                body: JSON.stringify({
+                error: error.message,
+            })
         })
-      })
     })
 }

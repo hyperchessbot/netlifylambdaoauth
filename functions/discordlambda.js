c9009d9b0b1662f0b4bcf28074f921f79ddc83ec
hyperchessbot <hyperchessbot@gmail.com>
2021-02-03 00:38:49 +0000

    Discord lambda

---
+++ functions/discordlambda.js
@@ -0,0 +1,42 @@
+const { update } = require('./octokit.js')
+
+const fetch = require('node-fetch')
+
+exports.handler = (event, context, callback) => {
+    /*if (event.httpMethod !== 'POST') {
+        return callback(null, {
+            statusCode: 400,
+            body: 'Method not allowed for this endpoint.',
+            headers: {
+                'Allow': 'POST'
+            }
+        })
+    }*/
+
+    const name = event.queryStringParameters.name;
+
+    console.log("name", name)
+
+    const path = `discordlambda/sites/${name}`
+
+    console.log("path", path)
+
+    const body = event.body
+
+    console.log("body", body)
+
+    update("hyperchessbot", "discordlambda", path, body, result => {
+      if(result.error) {
+        return callback(null, {
+            statusCode: 500,
+            body: `Update error.`
+        })
+      }
+      else {
+        return callback(null, {
+            statusCode: 200,
+            body: 'Updated ok.'
+        })
+      }
+    })        
+}
---
+++ functions/octokit.js
@@ -0,0 +1,92 @@
+const Octokit = require("@octokit/rest")
+
+const DEFAULT_OWNER     = "hyperchessbot"
+const DEFAULT_REPO      = "discordlambda"
+
+const fs = require('fs')
+
+const octokit = Octokit({
+    auth: process.env.HYPER_GITHUB_TOKEN,
+    userAgent: "Hyper Chess"
+})
+
+function putfile(ownerOpt, repoOpt, path, content, sha, callback){
+    let owner = ownerOpt || DEFAULT_OWNER
+    let repo = repoOpt || DEFAULT_REPO
+
+    console.log("put", owner, repo, path, "size", content.length, "sha", sha)
+
+    let contentb64 = new Buffer.from(content).toString("base64")
+
+    let req = {
+        owner: owner,
+        repo: repo,
+        path: path,
+        content: contentb64,        
+        message: path,
+        "commiter.name": "easychessanimations",
+        "commiter.email": "chessbotroulette@gmail.com",        
+    }
+
+    if(sha){
+        req.sha = sha
+    }
+
+    octokit.repos.createOrUpdateFile(req).then(
+        res => {
+            console.log("ok writing", owner, repo, path)
+            fs.writeFileSync("temp.txt", JSON.stringify(res, null, 2))
+            callback({
+                error: false,
+                status: `Written ${path} .`
+            })
+        },
+        err => {
+            console.log("failed writing", owner, repo, path)
+            fs.writeFileSync("temp.txt", JSON.stringify(err, null, 2))
+            callback({
+                error: true,
+                status: `Failed writing ${path} .`
+            })
+        }
+    )
+}
+
+function getfile(ownerOpt, repoOpt, path, callback, errcallback){
+    octokit.repos.getContents({
+        owner: ownerOpt || DEFAULT_OWNER,
+        repo: repoOpt || DEFAULT_REPO,
+        path: path        
+    }).then(
+        res => callback(res),
+        err => errcallback(err)
+    )
+}
+
+function getrepo(ownerOpt, repoOpt){
+    octokit.repos.get({
+        owner: ownerOpt || DEFAULT_OWNER,
+        repo: repoOpt || DEFAULT_REPO  
+    }).then(
+        res => console.log(res),
+        err => console.log(err)
+    )
+}
+
+function update(ownerOpt, repoOpt, UPDATE_PATH, content, callback){        
+    getfile(ownerOpt, repoOpt, UPDATE_PATH,
+        res => {
+            putfile(ownerOpt, repoOpt, UPDATE_PATH, content, res.data.sha, callback)
+        },
+        _ => {
+            putfile(ownerOpt, repoOpt, UPDATE_PATH, content, null, callback)
+        }
+    )
+}
+
+module.exports = {
+    DEFAULT_REPO: DEFAULT_REPO,
+    DEFAULT_OWNER: DEFAULT_OWNER,
+    getrepo: getrepo,
+    update: update
+}
